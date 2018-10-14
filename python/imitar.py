ff72ee46aacd840e0892efe53843a8a0b7e6964e
ferauche <thiago.ferauche@gmail.com>
2018-10-14 20:27:47 -0300

    scripts iniciais

---
+++ python/imitar.py
@@ -0,0 +1,6 @@
+#coding: latin1
+#''' código de testes para usar o opencv para imitar os movimentos'''
+#''' incompleto precisa implementar o códgio para configurar camera
+# realizar o reconhecimento dos movimentos'''
+
+import func.robo
